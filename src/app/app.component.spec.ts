753ff7d6e2f3f424e47dc0d935db4b2254c80675
mnr13098 <mutyamnarasimhareddy@gmail.com>
2025-02-11 10:30:20 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'people-manager' title`, () => {
+  it(`should have the 'Student-Portal' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('people-manager');
+    expect(app.title).toEqual('Student-Portal');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, people-manager');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Student-Portal');
   });
 });
